2ae915378c150cde5435cf92b1b4d960ca3ced2d
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-12-05 13:03:35 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import LoremContent from "./LoremContent";
 
 const App = () => {
   return (
-    <div>
+    <>
       <LoremContent />
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/components/LoremContent.js
+++ src/components/LoremContent.js
@@ -57,19 +57,33 @@ const LoremContent = () => {
     dispatch(fetchLorem());
   }, [dispatch]);
 
-  if (loading) return <h4 style={{ textAlign: "center", marginTop: "20px" }}>Loading news...</h4>;
-  if (error) return <h4 style={{ textAlign: "center", marginTop: "20px" }}>Error: {error}</h4>;
+  if (loading)
+    return (
+      <h4 style={{ textAlign: "center", marginTop: "20px" }}>
+        Loading news...
+      </h4>
+    );
+  if (error)
+    return (
+      <h4 style={{ textAlign: "center", marginTop: "20px" }}>Error: {error}</h4>
+    );
 
   return (
-    <div style={{ padding: "15px" }}>
-      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>A short Naration of Lorem Ipsum</h1>
-      <p style={{ textAlign: "center", marginBottom: "10px" }}>Below Contains A title and Body gotten froma random API, Please take your time to Review</p>
+    <>
+      <div style={{textAlign: "center"}}>
+        <h1>
+          A short Naration of Lorem Ipsum
+        </h1>
+        <h4 >
+          Below Contains A title and Body gotten from<br /> a random API, Please take your time to Review
+        </h4>
+      </div>
 
       <ul
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(2, 1fr)",
-          gap: "15px", 
+          gap: "15px",
           listStyle: "none",
           padding: 0,
           margin: 0,
@@ -81,17 +95,24 @@ const LoremContent = () => {
             style={{
               border: "1px solid #ccc",
               borderRadius: "8px",
-              padding: "10px", 
+              padding: "10px",
               backgroundColor: "#f9f9f9",
               boxShadow: "1px 1px 5px rgba(0,0,0,0.1)",
             }}
           >
-            <h4 className="title" style={{ marginBottom: "5px", fontSize: "16px" }}>Title :{item.title}</h4>
-            <p style={{ fontSize: "13px", lineHeight: "1.3", margin: 0 }}><b>Body:</b> {item.body}</p>
+            <h4
+              className="title"
+              style={{ marginBottom: "5px", fontSize: "16px" }}
+            >
+              <b>Title :</b>{item.title}
+            </h4>
+            <p className="body" style={{ fontSize: "13px", lineHeight: "1.3", margin: 0 }}>
+              <b>Body :</b>{item.body}
+            </p>
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
