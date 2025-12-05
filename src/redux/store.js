16a196088aa9113f1c1ba3e32c088b7a7abbaadc
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-12-05 14:03:10 +0530

    Updated

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import loremReducer from "./loremSlice";
-
-export const store = configureStore({
-  reducer: {
-    lorem: loremReducer,
-  },
-});
